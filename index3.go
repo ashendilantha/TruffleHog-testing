23c551e97a2fb5c474963290d0de1a78cad2f9b4
ashendilantha <ashendilantha@github.com>
2026-07-23 22:28:30 +0530

    feat: add index3.go

---
+++ index3.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("AWS_SECRET_ACCESS_KEY=wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY")
+	fmt.Println("SLACK_WEBHOOK=https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX")
+	fmt.Println("http://user:password@example.com")
+	fmt.Println("github_token=ghp_16charactertoken") 
+}
