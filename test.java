702e372575537f366b99adb8dd7adef1743ee081
ashendilantha <ashendilantha@github.com>
2026-07-23 23:33:36 +0530

    test

---
+++ test.java
@@ -0,0 +1,9 @@
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        String apiKey = "AIzaSyD-EXAMPLEKEY";
+        String webhookUrl = "https://hooks.slack.com/services/T00000000/B00000000/XXXXXXXXXXXXXXXXXXXXXXXX";
+        System.out.println("API Key: " + apiKey);
+        System.out.println("Webhook URL: " + webhookUrl);
+    }
+}
